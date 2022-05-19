b907d9d5d3da946247ef20e26cff1d0fbf8da10d
Phil <pnomolos@gmail.com>
2022-05-19 15:12:49 -0700

    Fixes for ruby 3, added github actions

--- spec/lazy_csv/malformed_spec.rb
+++ spec/lazy_csv/malformed_spec.rb
@@ -9,14 +9,10 @@ describe 'malformed_csv' do
     # TODO Should this be thrown on the initial parse rather than when you first begin reading?
     let(:csv_path) { "#{fixture_path}/malformed_header.csv" }
     it { should raise_error(CSV::MalformedCSVError) }
-    it { should raise_error(/(Missing or stray quote in line 1|CSV::MalformedCSVError)/) }
-    it { should raise_error(CSV::MalformedCSVError) }
   end
 
   context "malformed content" do
     let(:csv_path) { "#{fixture_path}/malformed.csv" }
-    it { should raise_error(CSV::MalformedCSVError) }
-    it { should raise_error(/(Missing or stray quote in line 1|CSV::MalformedCSVError)/) }
     it { should raise_error(CSV::MalformedCSVError) }
   end
 end
